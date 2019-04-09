122d444dcc11067e536b9a6dd4891e66326f8093
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-04-09 17:27:52 +0200

    added Bike

---
+++ src/main/java/edu/upc/dsa/Bike.java
@@ -0,0 +1,51 @@
+package edu.upc.dsa;
+
+public class Bike {
+    private String idBike;
+    private String description;
+    private double kms;
+    private String idStation;
+
+    public Bike(){
+
+    }
+
+    public Bike(String idBike, String description, double kms, String idStation) {
+        this.idBike = idBike;
+        this.description = description;
+        this.kms = kms;
+        this.idStation = idStation;
+    }
+
+    public String getIdBike() {
+        return idBike;
+    }
+
+    public void setIdBike(String idBike) {
+        this.idBike = idBike;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getKms() {
+        return kms;
+    }
+
+    public void setKms(double kms) {
+        this.kms = kms;
+    }
+
+    public String getIdStation() {
+        return idStation;
+    }
+
+    public void setIdStation(String idStation) {
+        this.idStation = idStation;
+    }
+}
