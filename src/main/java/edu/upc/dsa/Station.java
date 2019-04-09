591948a272ad4842a95ce28257fb7597ad286273
sheilalopez <47568080+sheilalopez@users.noreply.github.com>
2019-04-09 17:25:12 +0200

    added Station

--- src/main/java/edu/upc/dsa/Station.java
+++ src/main/java/edu/upc/dsa/Station.java
@@ -1,4 +1,61 @@
 package edu.upc.dsa;
 
 public class Station {
+    private String idStation;
+    private String description;
+    private int max;
+    private double lat;
+    private double lon;
+
+    public Station (){
+
+    }
+
+    public Station(String idStation, String description, int max, double lat, double lon) {
+        this.idStation = idStation;
+        this.description = description;
+        this.max = max;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getIdStation() {
+        return idStation;
+    }
+
+    public void setIdStation(String idStation) {
+        this.idStation = idStation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
 }
